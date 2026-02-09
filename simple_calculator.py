bd6ebcc04f16749175c24564f93bdc676ce76456
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2026-02-09 16:36:36 +0700

    add multiplication

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,7 +1,9 @@
 
 class Calculator:
-    pass
+    def multiply(self, a, b):
+        return a * b
 
 if __name__ == "__main__":
     calc = Calculator()
+    print("Multiplication: ", calc.multiply(10, 5))
     
