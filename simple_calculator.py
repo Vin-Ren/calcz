ae0c2bb35855fe75ae6aaca3df2d3e08874f470f
Roberto Eugenio Sugiarto <robertotoes12345@gmail.com>
2026-02-09 16:41:01 +0700

    add feat: divide

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,9 +1,9 @@
 
 class Calculator:
-    def multiply(self, a, b):
-        return a * b
+    def divide(self, a, b):
+        return a / b
 
 if __name__ == "__main__":
     calc = Calculator()
-    print("Multiplication: ", calc.multiply(10, 5))
+    print("Division: ", calc.divide(10, 5))
     
