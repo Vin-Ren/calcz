e1ee171fa68e4e52760d7b6a14bddde1bca10a39
BenedictZir <benedictusziraluo@gmail.com>
2026-02-09 16:39:18 +0700

    modulo

--- simple_calculator.py
+++ simple_calculator.py
@@ -2,7 +2,8 @@
 class Calculator:
     def multiply(self, a, b):
         return a * b
-
+    def modulo(self, a, b):
+        return a % b
 if __name__ == "__main__":
     calc = Calculator()
     print("Multiplication: ", calc.multiply(10, 5))
